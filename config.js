53fe4e9ccd4b7a110f1717a4df566ecd84cad333
Victor <49790026+VictorJG31@users.noreply.github.com>
2020-03-14 21:48:22 +0100

    update
    updated files, added features

---
+++ config.js
@@ -0,0 +1,13 @@
+
+
+
+module.exports = {
+
+    consumer_key: '6MAuTYYuy3w1FFpLO1srAAuev',
+    consumer_secret: 'D5tKb67HKZEoXebTHqskgSg11TmWYfCbiggQTXJbbXXgML47Nq',
+    access_token: '2635262321-CKeZHDirZCMdUspoQkZIlIVMPZanoDxwuIRXkb6',
+    access_token_secret: 'XeoEohC3E4Dd7gdsyOGBJaNI6FDRu1ny67h96hIQ4yYtF',
+    timeout_ms: 60 * 1000,
+    strictSSL: true
+}
+
